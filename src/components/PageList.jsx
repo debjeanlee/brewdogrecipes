2982b7c890d87333cda3149698cb20740bd9eb92
debjeanlee <debsgenes@gmail.com>
2020-11-02 23:05:26 +0800

    got abv sorted but dunno how to display

--- src/components/PageList.jsx
+++ src/components/PageList.jsx
@@ -6,14 +6,30 @@ function PageList(props) {
     // console.log("props page + 1" , props.page);
 
     function setPage(e){
-        props.setPage(e.target.innerHTML);
+        props.setPage(parseInt(e.target.innerHTML));
+    }
+
+    function nextPage(){
+        if (props.page === 13) {
+            console.log("HAHA NO MORE")
+        } else {
+            props.setPage(parseInt(props.page) + 1)
+        }
+    }
+
+    function prevPage(){
+        if (props.page === 1) {
+            console.log("HAHA NO MORE")
+        } else {
+            props.setPage(parseInt(props.page) - 1)
+        }
     }
 
 
     return (
         <Pagination>
             <Pagination.First onClick={() => {props.setPage(1)}}/>
-            <Pagination.Prev onClick={() => {props.setPage(props.page - 1)}}/>
+            <Pagination.Prev onClick={() => {prevPage()}}/>
             <Pagination.Item onClick={ e => {setPage(e)}}>{1}</Pagination.Item>
             <Pagination.Item onClick={ e => {setPage(e)}}>{2}</Pagination.Item>
             <Pagination.Item onClick={ e => {setPage(e)}}>{3}</Pagination.Item>
@@ -27,7 +43,7 @@ function PageList(props) {
             <Pagination.Item onClick={ e => {setPage(e)}}>{11}</Pagination.Item>
             <Pagination.Item onClick={ e => {setPage(e)}}>{12}</Pagination.Item>
             <Pagination.Item onClick={ e => {setPage(e)}}>{13}</Pagination.Item>
-            <Pagination.Next onClick={() => {props.setPage(props.page + 1)}}/>
+            <Pagination.Next onClick={() => {nextPage()}}/>
             <Pagination.Last onClick={() => {props.setPage(13)}}/>
         </Pagination>
     )
