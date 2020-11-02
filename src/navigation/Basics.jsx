db118c40af23451dce11fac9103298143e8490dd
debjeanlee <debsgenes@gmail.com>
2020-11-02 16:26:09 +0800

    basic info card component added

--- src/navigation/Basics.jsx
+++ src/navigation/Basics.jsx
@@ -1,16 +1,20 @@
-import React, {useState} from 'react'
+import React from 'react'
 import { Container } from 'react-bootstrap'
 import { brewInfo } from '../brewinginfo'
+import mashpot from '../images/mashpot.png'
+import boiling from '../images/boiling.png'
+import fermentation from '../images/fermentation.png'
+import conditioning from '../images/conditioning.png'
+import BasicInfoCard from '../components/BasicInfoCard'
 
 function Basics() {
 
-    // const [ info, setInfo ] = useState(brewInfo);
-
-    console.log("brewInfo:", brewInfo.mashing);
+    // console.log("brewInfo:", brewInfo.mashing);
     
     return (
         <Container className="text-center">
             <h1>BASICS</h1>
+            <BasicInfoCard />
         </Container>
     )
 }
