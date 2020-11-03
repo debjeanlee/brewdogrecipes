be6314d3a95e40adbd3c2d6259f3be9c692905f1
debjeanlee <debsgenes@gmail.com>
2020-11-03 12:45:25 +0800

    get beer by id works

--- src/beers/ID.jsx
+++ src/beers/ID.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from './axios.js'
+
 
 function ID() {
+
+    const {id} = useParams();
+    
+    const [ beer, setBeer ] = useState({});
+
+    useEffect(() => {
+  
+        async function getBeerData(){
+          const result = await axios.get(`?ids=${id}`);
+          setBeer(result.data);
+          }
+        
+        getBeerData();
+      
+    }, [])
+
+    console.log(beer);
 
     return (
         <div>
