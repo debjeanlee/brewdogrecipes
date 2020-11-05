390b74e0d03e635aae28b19ad1964ed4a138e834
debjeanlee <debsgenes@gmail.com>
2020-11-05 17:24:24 +0800

    basic sort for abv and ibu done

--- src/beers/All.jsx
+++ src/beers/All.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import BeerList from './BeerList'
-import { Row, Container } from 'react-bootstrap'
+import { Row, Container, Dropdown, DropdownButton } from 'react-bootstrap'
 import PageList from './PageList'
 
 function All(props) {
 
+    const [ state, setState ] = useState("all");
+
+    const [ beer, setBeer ] = useState(props.allBeer);
+    const [ abvLow, setAbv ] = useState([...beer].sort((a,b) => {return a.abv - b.abv}));
+    const [ ibuLow, setIbu ] = useState([...beer].sort((a,b) => {return a.ibu - b.ibu}));
+
+
+    // console.log(abvLow.sort((a,b) => {return a.abv - b.abv} ));
     
+
+    function handleClick(e){
+        console.log('clicked');
+        if (e.target.id === "abv"){
+            setState("abv");
+        } else if (e.target.id === "ibu"){
+            setState("ibu");
+        } 
+    }
+
+    
+    // console.log("beer", beer);
+
     return (
         <div>
             <Row className="d-flex justify-content-around mt-4">
@@ -15,22 +36,39 @@ function All(props) {
 
             <Container>
                 <Row>
-                    <div class="dropdown">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Sort by..
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">ABV</a>
-                            <a class="dropdown-item" href="#">IBU</a>
-                            <a class="dropdown-item" href="#">Year</a>
-                        </div>
-                    </div>
+                <DropdownButton 
+                    id="dropdown-basic-button" 
+                    title="Sort by.."
+                    variant="Secondary"
+                    >
+                    <Dropdown.Item onClick={handleClick} id="abv" >ABV</Dropdown.Item>
+                    <Dropdown.Item onClick={handleClick} id="ibu" >IBU</Dropdown.Item>
+                    <Dropdown.Item onClick={handleClick} id="year" >Year</Dropdown.Item>
+                </DropdownButton>
                 </Row>
             </Container>
 
-            <Row className="mt-3">
-                <BeerList allBeer={props.allBeer} page={props.page} setSingleBeer={props.setSingleBeer}/>
-            </Row>
+            {(state === "all") ? (
+                <Row className="mt-3">
+                    <BeerList allBeer={props.allBeer} page={props.page} setSingleBeer={props.setSingleBeer} />
+                </Row>
+            )
+            : "" }
+
+            {(state === "abv") ? (
+                <Row className="mt-3">
+                    <BeerList allBeer={abvLow} page={props.page} setSingleBeer={props.setSingleBeer} />
+                </Row>
+            )
+            : "" }
+
+            {(state === "ibu") ? (
+                <Row className="mt-3">
+                    <BeerList allBeer={ibuLow} page={props.page} setSingleBeer={props.setSingleBeer} />
+                </Row>
+            )
+            : "" }
+        
             {/* <Row className="d-flex justify-content-end mt-4 col-10">
                 <PageList />
             </Row> */}
